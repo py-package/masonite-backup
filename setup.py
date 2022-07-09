8e2c9ccc25d290952376a728dbcffa2f907e24a5
Yubaraj Shrestha <companion.krish@outlook.com>
2022-07-09 12:27:31 +0545

    Bumped Version to 0.0.2

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.1",
+    version="0.0.2",
     packages=[
         "backup",
+        "backup.commands",
         "backup.providers",
         "backup.config",
     ],
@@ -35,7 +36,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         # Indicate who your project is intended for
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
